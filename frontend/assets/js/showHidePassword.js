22e09a338e17f57eb633dd40f8a3ec03eeca1e52
gonca <goncalvesrodrigo.rg@gmail.com>
2023-04-14 19:12:57 +0100

    Updated login

---
+++ frontend/assets/js/showHidePassword.js
@@ -0,0 +1,8 @@
+const showHidePassword = () => {
+    let password = document.getElementById("password");
+    if (password.type === "password") {
+        password.type = "text";
+        } else {
+    password.type = "password";
+    }
+}

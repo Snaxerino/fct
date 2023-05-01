c2c701a4821052c34000519f06a979f57140b0b5
gonca <goncalvesrodrigo.rg@gmail.com>
2023-05-01 23:06:19 +0100

    Updated

--- frontend/assets/js/links.js
+++ frontend/assets/js/links.js
@@ -1,4 +1,5 @@
-const paginaLogin = () => location.href = "frontend/login.html";
+const paginaLogin = () => location.href = "frontend/login.html"; // Home para Login
+const paginaLogin2 = () => location.href = "../frontend/login.html"; // Contribuidores para Login
 const voltarParaMenu = () => location.href = "../index.html";
 const verVideo = () => location.href = "https://www.youtube.com/channel/UCdlo76ck9sVojRF8Nm3HYcg";
 // Apenas temporário e para efeito de testes

686d3df083b568905c9fda3e3d6a0b158f16ea7a
gonca <goncalvesrodrigo.rg@gmail.com>
2023-05-06 16:41:40 +0100

    Updated Nome da página renomeada ficheiro geral js Melhoria ao fazer zoom

--- frontend/assets/js/links.js
+++ frontend/assets/js/links.js
@@ -3,4 +3,4 @@ const paginaLogin2 = () => location.href = "../frontend/login.html"; // Contribu
 const voltarParaMenu = () => location.href = "../index.html";
 const verVideo = () => location.href = "https://www.youtube.com/channel/UCdlo76ck9sVojRF8Nm3HYcg";
 // Apenas temporário e para efeito de testes
-const paginaPrivada = () => location.href = "../backend/main.html";
+const paginaPrivada = () => location.href = "../backend/perfil.html";
--- frontend/assets/js/mostrarEsconderPassword.js
+++ frontend/assets/js/mostrarEsconderPassword.js
@@ -5,7 +5,7 @@ const mostrarEsconderPassword = () => {
       password.type = "text";
       icon.classList.remove("fa-eye");
       icon.classList.add("fa-eye-slash");
-    } else {
+    } else {  
       password.type = "password";
       icon.classList.remove("fa-eye-slash");
       icon.classList.add("fa-eye");
